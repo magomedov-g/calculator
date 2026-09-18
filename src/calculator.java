4f1a8b4f1dd4fec63c2dc3f35804b78131cedeb8
Ghazimuhammad <gazimagomedm054@gmail.com>
2026-09-18 19:22:41 +0300

    character input symbol

--- src/calculator.java
+++ src/calculator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class calculator {
+    public static int CharacterInput(int a, int b, String symbol) {
+        switch (symbol) {
+            case "*":
+                return (a * b);
+            case "/":
+                return (a / b);
+            case "+":
+                return (a + b);
+            case "-":
+                return (a - b);
+            default:
+            throw new IllegalStateException("Invalid input: ");
+        }
+    }
 }
